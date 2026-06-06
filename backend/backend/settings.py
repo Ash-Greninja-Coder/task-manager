05507b8a945245cc32ea2e3ea4d196a552c4de60
Ash-Greninja-Coder <160762718+Ash-Greninja-Coder@users.noreply.github.com>
2026-06-06 15:31:54 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,6 +76,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://task-manager-frontend.onrender.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
